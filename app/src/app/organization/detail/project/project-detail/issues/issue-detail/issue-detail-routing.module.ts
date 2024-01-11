494032ee08c8145663e4b06ead139c55fa8b14a1
Phan Van Thien <thien.phanvan9846@gmail.com>
2024-01-11 16:39:58 +0700

    add app

--- app/src/app/organization/detail/project/project-detail/issues/issue-detail/issue-detail-routing.module.ts
+++ app/src/app/organization/detail/project/project-detail/issues/issue-detail/issue-detail-routing.module.ts
@@ -6,8 +6,8 @@ import { IssueDetailPage } from './issue-detail.page';
 const routes: Routes = [
   {
     path: '',
-    component: IssueDetailPage
-  }
+    component: IssueDetailPage,
+  },
 ];
 
 @NgModule({
